cdb64a1c30e9c3e582fe34368270c497e83af25d
Morten Salvesen <msalvefirm@gmail.com>
2019-06-17 19:12:56 +0200

    Revert "Commented"
    This reverts commit b971eb0411cf243aba82d76a61b028e9e0c5a230.
    
    Rollback to day 1

--- app.rb
+++ app.rb
@@ -1,10 +1,8 @@
-require_relative 'router'
 require_relative 'recipe'
 require_relative 'cookbook'    # You need to create this file!
 require_relative 'controller'  # You need to create this file!
 require_relative 'view'
-require_relative 'scrape_recipe_service'
-
+require_relative 'router'
 
 csv_file   = File.join(__dir__, 'recipes.csv')
 cookbook   = Cookbook.new(csv_file)
@@ -12,4 +10,5 @@ controller = Controller.new(cookbook)
 
 router = Router.new(controller)
 
+# Start the app
 router.run
--- cookbook.rb
+++ cookbook.rb
@@ -18,52 +18,30 @@ class Cookbook
   end
 
   def remove_recipe(index)
-    @recipes.delete_at(index)
-    save_to_csv
+     @recipes.delete_at(index)
+     save_to_csv
   end
 
   def mark_as_done(index)
-    # We find our recipe, and mark it as done
     @recipes[index].mark_as_done!
-
-    # Then we save/update our CSV.
     save_to_csv
   end
 
   private
 
   def load_csv_file
-    # headers: :first_row => The first row in the CSV is our headers
-    # header_converters: :symbol => Turn the headers into Symbols instead of Strings
     CSV.foreach(@csv_file_path, headers: :first_row, header_converters: :symbol) do |row|
-
-      # All the information loaded from our CSV are Strings.
-      # We need to convert that to a boolean.
-      # Any boolean expression:
-      # `a > b` or `a == b`
-      # is evaluated to either true or false.
-      # So `row[:done] == "true"` is either true or false.
+      # byebug
       row[:done] = row[:done] == "true"
-
-      # Our Recipe.new takes a hash as an argument, and because
-      # we've converted our headers we can now simply pass the entire row.
       @recipes << Recipe.new(row)
     end
   end
 
   def save_to_csv
     CSV.open(@csv_file_path, 'wb') do |csv|
-
-      # The header needs to be written to the CSV first.
       csv << ['name', 'description', 'prep_time', 'done']
-
-      # Then we can write each recipe.
       @recipes.each do |recipe|
-
-        # Instead of writing:
-        # [recipe.name, recipe.description, recipe.prep_time, recipe.done?]
-        # We use our Recipe#to_csv helper function.
-        csv << recipe.to_csv
+        csv << [recipe.name, recipe.description, recipe.prep_time, recipe.done?]
       end
     end
   end
